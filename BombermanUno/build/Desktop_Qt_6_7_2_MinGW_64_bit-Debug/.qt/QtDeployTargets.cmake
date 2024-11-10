d7fbfdcecad7e9159e7d5768506bde37aec6561c
Eder-PoloH <eder.polo@udea.edu.co>
2024-11-10 12:44:39 -0500

    Inicio base del juego bomberman
    Signed-off-by: Eder-PoloH <eder.polo@udea.edu.co>

---
+++ BombermanUno/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_BombermanUno_FILE D:/ASUS/Documents/UdeA/TELECOMUNICACIONES_NV_2/INFORMATICA_II/LABORATORIO/Practica_04/BombermanUno/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/BombermanUno.exe)
+set(__QT_DEPLOY_TARGET_BombermanUno_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_BombermanUno_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
